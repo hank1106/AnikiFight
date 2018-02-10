f4682889daf7a3f93bf61868831f53c212cfe2a7
ranf999 <fengran_i@didichuxing.com>
2018-02-09 16:14:29 -0800

    StatusCheck

--- Assets/Scripts/DumbAI.cs
+++ Assets/Scripts/DumbAI.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
+using SC;
+
+
+
+
 public class DumbAI : MonoBehaviour {
 
 	private Rigidbody2D rg2d;
 	private bool leftTurn = false;
 	private bool rightTurn = true;
 	private int countCombo = 0;
+    private bool waiting = false;
+    private float start_wating_time;
+
+    private const float WAITING_TIME = 3.0f;
+    private const int WAIT_FOR_A_WHILE = 0;
+    
+
+    //Random random = new Random();
 	
 	
 	public AudioSource[] arrAllAudioSource;
@@ -22,69 +37,22 @@ public class DumbAI : MonoBehaviour {
 	private void SetForce(float ttt) {
 		this.force = ttt;
 	}
-	
-	private void PositionCheck(float playerX, float playerY, float AIX, float AIY) {
-		float distance = (AIX - playerX) * (AIX - playerX) + (AIY - playerY) * (AIY - playerY);
-		
-		if (AIX - playerX > 2f) {
-			if (rg2d.velocity.x < 3f) {
-				rg2d.velocity = new Vector2 (-3f, rg2d.velocity.y);
-				if (leftTurn) {
-					 rg2d.transform.localScale = new Vector2(-rg2d.transform.localScale.x, rg2d.transform.localScale.y);
-					leftTurn = false;
-					rightTurn = true;
-				}
-				
-			}
-		}
-		if (distance < 4f) {
-			rg2d.velocity = new Vector2 (0, rg2d.velocity.y);
-			
-			Combat();
-		}
 
-       if (AIX - playerX < -2f) {
-		   if (rg2d.velocity.x > -3f) {
-				rg2d.velocity = new Vector2 (3f, rg2d.velocity.y);
-			   if (rightTurn) {
-					 rg2d.transform.localScale = new Vector2(-rg2d.transform.localScale.x, rg2d.transform.localScale.y);
-					rightTurn = false;
-					leftTurn = true;
-				}
-			   
-			}
-	   }
-	}
-	
-	private void StatusCheck() {
-		
-		float playerX = GameObject.Find("Aniki").transform.position.x;
-		float AIX = GameObject.Find("Enemy").transform.position.x;
-		float playerY = GameObject.Find("Aniki").transform.position.y;
-		float AIY = GameObject.Find("Enemy").transform.position.y;
-		
-		if (!BeingHitCheck()) {
-			
-			PositionCheck(playerX, playerY, AIX, AIY);
-			
-		}
-	}
-	
-	private bool BeingHitCheck() {
-		return false;
-	}
-	
-	private void Combat() {
-		if (countCombo < 2) {
-				anim.SetTrigger("j");
-				countCombo ++;
-			}else{
-				anim.SetTrigger("k");
-			}
-	}
+    public void Combat()
+    {
+        if (countCombo < 2)
+        {
+            anim.SetTrigger("j");
+            countCombo++;
+        }
+        else
+        {
+            anim.SetTrigger("k");
+        }
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		anim = GetComponent<Animator>();
 		rg2d = GetComponent<Rigidbody2D> (); 
 
@@ -99,12 +67,78 @@ public class DumbAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-//		float playerX = GameObject.Find("Aniki").transform.position.x;
-//		float AIX = GameObject.Find("Enemy").transform.position.x;
-//		float playerY = GameObject.Find("Aniki").transform.position.y;
-//		float AIY = GameObject.Find("Enemy").transform.position.y;
-		
-		StatusCheck();
+        float playerX = GameObject.Find("Aniki").transform.position.x;
+        float AIX = GameObject.Find("Enemy").transform.position.x;
+        float playerY = GameObject.Find("Aniki").transform.position.y;
+        float AIY = GameObject.Find("Enemy").transform.position.y;
+        if(waiting == true)
+        {
+            if (Time.time - start_wating_time >= WAITING_TIME)
+            {
+                waiting = false;
+            }
+        }
+
+
+        if (waiting == false) {
+            if (AIX - playerX > 2f)
+            {
+                if (rg2d.velocity.x < 3f)
+                {
+                    rg2d.velocity = new Vector2(-3f * Random.Range(0.1f, 0.7f), rg2d.velocity.y);
+                    if (leftTurn)
+                    {
+                        rg2d.transform.localScale = new Vector2(-rg2d.transform.localScale.x, rg2d.transform.localScale.y);
+                        leftTurn = false;
+                        rightTurn = true;
+                    }
+
+                }
+
+            }
+
+            if (AIX - playerX < -2f)
+            {
+                if (rg2d.velocity.x > -3f)
+                {
+                    rg2d.velocity = new Vector2(3f * Random.Range(0.1f, 0.7f), rg2d.velocity.y);
+                    if (rightTurn)
+                    {
+                        rg2d.transform.localScale = new Vector2(-rg2d.transform.localScale.x, rg2d.transform.localScale.y);
+                        rightTurn = false;
+                        leftTurn = true;
+                    }
+
+                }
+            }
+
+        }
+        
+        
+
+        switch(CheckStatus())
+        {
+            case WAIT_FOR_A_WHILE:
+                if(waiting == true)
+                {
+                    if(Time.time - start_wating_time >= 2)
+                    {
+                        waiting = false;
+                    }
+                }
+                else
+                {
+                    waiting = true;
+                    start_wating_time = Time.time;
+                }
+                
+
+                //rg2d.velocity = new Vector2(0, rg2d.velocity.y);
+                break;
+            default:
+                break;
+        }
+        //StatusCheck();
 		
 		//float distance = (AIX - playerX) * (AIX - playerX) + (AIY - playerY) * (AIY - playerY);
 
@@ -148,5 +182,22 @@ public class DumbAI : MonoBehaviour {
            
 
 	}
+
+    int CheckStatus() {
+        float playerX = GameObject.Find("Aniki").transform.position.x;
+        float AIX = GameObject.Find("Enemy").transform.position.x;
+        float playerY = GameObject.Find("Aniki").transform.position.y;
+        float AIY = GameObject.Find("Enemy").transform.position.y;
+
+        if (StatusCheck.BeingHitCheck())
+        {
+            return 0;
+        }
+        else
+        {
+
+        }
+        return -1;
+    }
 	
 }
---
+++ Assets/Scripts/StatusCheck.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace SC {
+
+    public class StatusCheck
+    {
+
+        private const float LIGHT_ATTACK = 2.5f;
+
+        public static bool PositionCheck(float playerX, float playerY, float AIX, float AIY, Rigidbody2D rg2d)
+        {
+            float distance = (AIX - playerX) * (AIX - playerX) + (AIY - playerY) * (AIY - playerY);
+
+            if (distance < 4f)
+            {
+                return true;
+                // TODO: speed to 0
+            }
+
+            return false;
+
+        }
+
+        public static bool CheckStatus()
+        {
+
+            float playerX = GameObject.Find("Aniki").transform.position.x;
+            float playerY = GameObject.Find("Aniki").transform.position.y;
+            float AIX = GameObject.Find("Enemy").transform.position.x;
+            float AIY = GameObject.Find("Enemy").transform.position.y;
+
+
+
+
+
+            return false;
+        }
+
+        public static bool BeingHitCheck()
+        {
+            float playerX = GameObject.Find("Aniki").transform.position.x;
+            float playerY = GameObject.Find("Aniki").transform.position.y;
+            float AIX = GameObject.Find("Enemy").transform.position.x;
+            float AIY = GameObject.Find("Enemy").transform.position.y;
+
+            float distance = Mathf.Sqrt((AIX - playerX) * (AIX - playerX) + (AIY - playerY) * (AIY - playerY));
+
+            if (Input.GetKeyDown(KeyCode.J) && distance <= LIGHT_ATTACK)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
