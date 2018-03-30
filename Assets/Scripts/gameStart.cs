a76799a9548ac3367005391af2d790fcc025e3b6
Yuqian Chen <yuqianchen@guest-wireless-upc-1601-10-120-027-159.usc.edu>
2018-03-30 14:23:36 -0700

    Starting page

---
+++ Assets/Scripts/gameStart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gameStart : MonoBehaviour {
+
+	public void load(string name){
+		SceneManager.LoadScene(name);
+	}
+}
